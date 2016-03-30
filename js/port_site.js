5f82ed942270f15123116489ab3ed5e297cf91db
Thomas B <thomas.bae0@gmail.com>
2016-03-29 22:32:06 -0400

    changing , editing refactoring. need to find current non deprecated code and build on it

--- js/port_site.js
+++ js/port_site.js
@@ -5,17 +5,17 @@ $(document).ready(function(){
    $('section[data-type="background"]').each(function(){
      // declare the variable to affect the defined data-type
      var $scroll = $(this);
-                     
+     
       $(window).scroll(function() {
-        // HTML5 proves useful for helping with creating JS functions!
-        // also, negative value because we're scrolling upwards                             
+         //need to refactor this to use non deprecated version
+         //compare to own created solution
         var yPos = -($window.scrollTop() / $scroll.data('speed')); 
          
         // background position
         var coords = '50% '+ yPos + 'px';
  
         // move the background
-        $scroll.css({ backgroundPosition: coords });    
-      }); // end window scroll
-   });  // end section function
-}); // close out script
+    
+      });
+   }); 
+}); 
